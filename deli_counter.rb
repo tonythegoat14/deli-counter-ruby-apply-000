72d9df01dda07e4acdd18fd557d32322ed6b3579
Antonnio davis <tonythegoat14@icloud.com>
2019-08-28 23:12:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,22 +5,12 @@ def take_a_number(katz_deli, name)
  
  def line(katz_deli) 
    
-    line(katz_deli) #=> "The line is currently: 1. Tony 2. Peter 3. Dame 4. Ash"
-  line(katz_deli) #=> "The line is currently: 1. Peter 2. Dame 3. Ash"
-   line(katz_deli) #=> "The line is currently:  1. Dame 2. Ash"
-  line(katz_deli) #=> "The line is currently:  1. Ash 2. Dre"
- line(katz_deli) #=> "The line is currently:  1. Dre"
- line(katz_deli) #=> "The line is currently empty"
-    
+ 
  end 
  
  def now_serving(katz_deli)
    
-   now_serving(katz_deli) #=> "currently serving Tony"
-now_serving(katz_deli) #=> "currently serving Peter"
-now_serving(katz_deli) #=> "currently serving Dame"
-now_serving(katz_deli) #=> "currently serving Ash"
-now_serving(katz_deli) #=> "currently serving Dre"
+  
 
  end 
    
